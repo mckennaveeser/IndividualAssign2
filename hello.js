0c1da05d89afca258d2df9aec121f81131bd6286
mckennaveeser <veesem38@uwosh.edu>
2020-11-28 01:44:09 +0000

    Added package.json, PORT changes, and procfile

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
 	var q = url.parse(req.url, true);
@@ -20,7 +21,7 @@ fs.readFile(filename, function(err, data) {
 	res.write(data);
 	return res.end();
 	});
-}).listen(8080);
+}).listen(PORT);
 
-console.log("Server Listening on Port 8080...");
+console.log("Server Listening on assigned Port...");
  
